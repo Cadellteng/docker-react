7bda376688d9201ffc9cbda8753cfcdee99c6316
Cadellteng <tengbangwei@gmail.com>
2021-11-08 14:38:29 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Oh shit! React suck~ It changed!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don't learn React! Woohoo!
         </a>
       </header>
     </div>
